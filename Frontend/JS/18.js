498093f8970ac4bf04d1b1be524758e1725042f3
aman-codezz <mdamanan558@gmail.com>
2026-09-16 23:05:29 +0530

    Async/await funciton practice

--- Frontend/JS/18.js
+++ Frontend/JS/18.js
@@ -1,1 +1,13 @@
-console.log("helllo world");
+async function first(a) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(a);
+            resolve(a)
+        }, 1000);
+    })
+}
+async function main() {
+    await first("hello")
+    console.log("world")
+}
+main()
