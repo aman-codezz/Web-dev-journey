3092eeb14911271b2bad30aae0a2e5418333ed4f
aman-codezz <mdamanan558@gmail.com>
2026-09-17 22:40:18 +0530

    fix async function

--- Frontend/JS/18.js
+++ Frontend/JS/18.js
@@ -1,5 +1,5 @@
 async function first(a) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         setTimeout(() => {
             console.log(a);
             resolve(a)
