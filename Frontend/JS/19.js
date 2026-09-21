89a7410259f6fd8774b6642c249cf84fd7b59394
aman-codezz <mdamanan558@gmail.com>
2026-09-21 22:37:42 +0530

    JS revision

--- Frontend/JS/19.js
+++ Frontend/JS/19.js
@@ -1,7 +1,7 @@
 let container = document.querySelector(".container")
 container.style.backgroundColor = "red"
 let box = document.querySelector(".box1")
-box.addEventListener("click",(e)=>{
+box.addEventListener("click", function(e){
     console.log(e.currentTarget.parentElement)
     
 })
