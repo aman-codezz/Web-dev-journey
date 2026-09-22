f735a390f5e3555585cb316d22ffe44edde01b43
aman-codezz <mdamanan558@gmail.com>
2026-09-22 22:17:43 +0530

    practice js

--- Frontend/JS/19.js
+++ Frontend/JS/19.js
@@ -5,3 +5,11 @@ box.addEventListener("click", function(e){
     console.log(e.currentTarget.parentElement)
     
 })
+document.addEventListener("click", function(e){
+    console.log(e.clientX, e.clientY)
+})  
+
+let letter = document.querySelector("#letter")
+letter.addEventListener("keydown", (e)=>{
+    console.log(e.key)
+})
