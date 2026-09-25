606f4a5ea7eaf46659125732dc99ceebc0a2cdf3
aman-codezz <mdamanan558@gmail.com>
2026-09-25 22:04:39 +0530

    callback function rev

--- Frontend/JS/20.js
+++ Frontend/JS/20.js
@@ -1,7 +1,32 @@
-let container = document.querySelector(".container")
-function main(){
-    setInterval(() => {
-        container.style.backgroundColor="green"
-    }, 2000);
+Array.prototype.myfilter = function(callback) {
+    let result = []
+    for(let i=0; i<this.length; i++){
+        if (callback(this[i], i, this)) {
+            result.push(this[i])
+        }
+    }
+    return result
 }
-main()
+let numbers = [10,20,30,40,50,60]
+let greaterThanTwenty= numbers.myfilter((num)=>
+    {num > 20})
+console.log(greaterThanTwenty)
+// Custom implementation of Array.prototype.myFilter
+// Array.prototype.myFilter = function (callback) {
+//     const result = [];
+
+//     // 'this' refers to the array calling myFilter
+//     for (let i = 0; i < this.length; i++) {
+//         // We pass (currentValue, index, array) just like native Array methods
+//         if (callback(this[i], i, this)) {
+//             result.push(this[i]);
+//         }
+//     }
+
+//     return result;
+// };
+
+// const numbers = [10, 25, 30, 45, 50];
+// const greaterThanTwenty = numbers.myFilter((num) => num > 20);
+
+// console.log(greaterThanTwenty); // [25, 30, 45, 50]
