16c95919692b25192ee0f7651561bace1bacdd37
aman-codezz <mdamanan558@gmail.com>
2026-09-26 17:49:45 +0530

    calculator using callback fucniton

--- Frontend/JS/20.js
+++ Frontend/JS/20.js
@@ -1,32 +1,7 @@
-Array.prototype.myfilter = function(callback) {
-    let result = []
-    for(let i=0; i<this.length; i++){
-        if (callback(this[i], i, this)) {
-            result.push(this[i])
-        }
-    }
-    return result
+function calculate(a,b, operation){
+    return operation(a,b)
 }
-let numbers = [10,20,30,40,50,60]
-let greaterThanTwenty= numbers.myfilter((num)=>
-    {num > 20})
-console.log(greaterThanTwenty)
-// Custom implementation of Array.prototype.myFilter
-// Array.prototype.myFilter = function (callback) {
-//     const result = [];
-
-//     // 'this' refers to the array calling myFilter
-//     for (let i = 0; i < this.length; i++) {
-//         // We pass (currentValue, index, array) just like native Array methods
-//         if (callback(this[i], i, this)) {
-//             result.push(this[i]);
-//         }
-//     }
-
-//     return result;
-// };
-
-// const numbers = [10, 25, 30, 45, 50];
-// const greaterThanTwenty = numbers.myFilter((num) => num > 20);
-
-// console.log(greaterThanTwenty); // [25, 30, 45, 50]
+let result1 = calculate(parseInt(prompt()), parseInt(prompt()), function sum(x,y){ return x + y})
+let result2 = calculate(parseInt(prompt()),parseInt(prompt()), function multiple(c,d){return c*d})
+console.log(result1)
+console.log(result2)
